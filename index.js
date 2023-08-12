660541b5ca01da9746020a4df676698a87d016e1
eraltafs <43698833+eraltafs@users.noreply.github.com>
2023-08-12 09:38:41 +0530

    created get api for index file for client side

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ const app = express();
 
 app.use(cors())
 app.use(express.json());
+app.use(express.static(__dirname + "/public"));
+
+// get Api for Frontend
+app.get("/", (req, res) => {
+  res.sendFile(__dirname, "public", "index.html");
+});
 
 // post api for sending json 
 app.post("/submit", (req, res) => {
